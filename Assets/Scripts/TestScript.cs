24124792aa9c48f5b861665496a7e5e659340d33
Hoss Shah <hossein.shbz@gmail.com>
2020-06-27 01:17:15 -0700

    initial commit

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] InputField _inputLongitude;
+    [SerializeField] InputField _inputLatitude;
+    [SerializeField] Text _textOutput;
+    public void OnButtonClick()
+    {
+        float lon, lat;
+        float.TryParse(_inputLatitude.text, out lat);
+        float.TryParse(_inputLongitude.text, out lon);
+        var rg = GetComponent<ReverseGeocoder>();
+        string country = rg.ReverseGeocodeCountry(lon, lat);
+        string usstate = rg.ReverseGeocodeUSAState(lon, lat);
+
+        _textOutput.text = $"Country: '{country}'\nState: '{usstate}'";
+    }
+}
